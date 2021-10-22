a6bf718870f529e6fa232e6c7705c90b8d510c7f
Ravelfett <ravefett@gmail.com>
2021-10-22 13:33:35 +0200

    clean

--- mefait #2/mefait.py
+++ mefait #2/mefait.py
@@ -1,3 +1,4 @@
+import os
 import random
 bible=open('!THE HOLY BIBLE.txt','rb')
 t=bible.read()
@@ -12,11 +13,17 @@ def convert(v:bytes):
         if char in mathieu:
             rend+=char
     return rend[2:]
+
 def create(nb):
     for _ in range(nb):
         title=convert(random.choice(verses))
         while len(title)>252:
             title=convert(random.choice(verses))
-        file=open(title+".txt","w")
+        file=open("./denis-saint-benois-de-moselle/"+title+".txt","w")
         file.write("Alexis derail préfère la NSI à la SI")
         file.close()
+
+if not os.path.exists("./denis-saint-benois-de-moselle"):
+  os.mkdir("./denis-saint-benois-de-moselle")
+
+create(50)
--- mefait #3/alexiss.py
+++ mefait #3/alexiss.py
@@ -1,3 +1,4 @@
+import os
 import random
 filemig=open('alexis en 116x116.bmp','rb')
 derail=filemig.read()
@@ -7,33 +8,32 @@ miniderail=filedrail.read()
 filedrail.close()
 a = 2
 
+if not os.path.exists("./denis-saint-benois-de-moselle"):
+  os.mkdir("./denis-saint-benois-de-moselle")
+
 title=""
 for i in range(10):title+=random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-rend=open('./essais/'+title+'title.bmp','ab')
+rend=open('./denis-saint-benois-de-moselle/'+title+'title.bmp','ab')
 
 
 entete=derail[:54]
 derail=derail[54:]
 
 
-def tobyte(nb:int):
-    he=hex(nb)[2:]
-    while len(he)<2:
-        he="0"+he
-    return bytes.fromhex(he)
-
 def tocolored(coul:tuple,toma=miniderail):
-    tabl=[[]]
-    ow = int.from_bytes(toma[18:22], byteorder='little')
+    tabl=[]
+    oww = int.from_bytes(toma[18:22], byteorder='little')
     toma=toma[54:]
+    c = []
     for i in range(0,len(toma),3):
         truc=(toma[i]+toma[i+1]+toma[i+2])/(3*255)
-        for couluer in coul[::-1]:
-            tabl[-1].append(tobyte(int(truc*couluer)))
-        if i%16==0 and i!=0:
-            tabl.append([])
+        if i%(oww*3)==0 and i!=0:
+            tabl.append(c)
+            c = []
+        c.append(bytes([int(truc*coul[0]), int(truc*coul[1]), int(truc*coul[2])]))
+    tabl.append(c)
     return tabl
-            
+
 
 ow = int.from_bytes(entete[18:22], byteorder='little')
 oh = int.from_bytes(entete[22:26], byteorder='little')
@@ -43,14 +43,14 @@ nw = w.to_bytes(4, byteorder="little")
 nh = h.to_bytes(4, byteorder="little")
 
 
-"""for i in range(54):
+for i in range(54):
     if i>=18 and i<26:
         if i<22:
             rend.write(bytes([nw[i-18]]))
         else:
             rend.write(bytes([nh[i-22]]))
     else:
-        rend.write(bytes([entete[i]]))"""
+        rend.write(bytes([entete[i]]))
 
 
 
@@ -58,9 +58,9 @@ def getpixel(x, y):
     return derail[3*(x+y*ow)],derail[3*(x+y*ow)+1],derail[3*(x+y*ow)+2]
 
 
-"""for i in range(h):
+for i in range(h):
     for j in range(w):
         c=tocolored(getpixel(int(j/a), int(i/a)))
-        rend.write(bytes(getpixel(int(j/a), int(i/a))))"""
+        rend.write(c[(i)%16][(j)%16])
 
 rend.close()
