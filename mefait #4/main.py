6dcba2f4816cb8a3dfc569f145413ff1f3283dc5
Ravelfett <ravefett@gmail.com>
2021-12-02 21:24:13 +0100

    pa amato

--- mefait #4/main.py
+++ mefait #4/main.py
@@ -1,35 +1,38 @@
-import os,csv
+import os, json
+import random
 dos=lambda path: os.mkdir(path)
-file=lambda path : os.system(f'echo pas de solution ici >> "{path}.txt"')
+def file(path):
+    f = open(path+".txt", "a")
+    f.write("https://bit.ly/IqT6zt")
+    f.close()
 
-def parcours():
-    " Parcours des data, renvoie la liste "
-    qandr=open('qandr.csv')
-    ligne=qandr.readline()
-    while ligne:
-        yield ligne.split(sep=';')
-        ligne=qandr.readline()[:-1]
-    qandr.close()
-    
+with open('paths.json') as f:
+  RJOHICQSBKJRHKUSHRJLIQZEROIHU = json.load(f)
+
+a = random.randrange(10, 30)
+b = random.randrange(10, 30)
+nq = {"question": ("Sa fait combien"+str(a)+"*"+str(b)),
+"anwsers": [str(i) for i in range(0, 1000)],
+"correct": a*b}
+RJOHICQSBKJRHKUSHRJLIQZEROIHU["questions"].append(nq)
+
+nq = {"question": "Amato?",
+"anwsers": ["Amato"+"⁈"*i for i in range(0, 50)]+["pa Amato"],
+"correct": 50}
+RJOHICQSBKJRHKUSHRJLIQZEROIHU["questions"].append(nq)
+
 dos("./res")
 path='./res/'
-for brain in parcours():
-    # L'indice de la solution
-    good=int(brain[-1])
-    brain.pop()
-
+for KLEZRJLKHSDHJLKETRHGUERIEHIUEZKJERQ in RJOHICQSBKJRHKUSHRJLIQZEROIHU["questions"]:
     # La question en titre de fichier text
-    file(path+brain[0])
-    brain.pop(0)
-    
-    for ind,content in enumerate(brain):
-        dos(path+content)
-        if ind!=good:
-            os.system('cp hom.lnk '+path+content+"/fin.lnk")
-            # renvoyer vers le début si on se trompe,
-            # la vraie solution finale sera aussi accesible
-            # seulement par un fichier fin.lnk
-    
+    file(path+KLEZRJLKHSDHJLKETRHGUERIEHIUEZKJERQ["question"])
+
+    OERIJZEOZRJIERZEORPJIKJOEIZR = KLEZRJLKHSDHJLKETRHGUERIEHIUEZKJERQ["anwsers"][KLEZRJLKHSDHJLKETRHGUERIEHIUEZKJERQ["correct"]]
+
+    for i in KLEZRJLKHSDHJLKETRHGUERIEHIUEZKJERQ["anwsers"]:
+        dos(path+i)
+        if i != OERIJZEOZRJIERZEORPJIKJOEIZR:
+            os.system('cp hom.lnk "'+os.path.join(path,i)+'/fin.lnk"')
+
     # continuer à la prochaine descendance
-    path+=brain[good]+"/"
-    
+    path+=OERIJZEOZRJIERZEORPJIKJOEIZR+"/"
