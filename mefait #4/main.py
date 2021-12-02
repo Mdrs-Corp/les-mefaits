9db356c88d92b61821a4300ac6c34fb1ee61a516
momoladebrouill <momoladebrouill@gmail.com>
2021-12-02 14:30:54 +0100

    dernière modif
    j'ai trouvé commment faire un raccourci, et on peut le copier au bon endroit avec `cp`

--- mefait #4/main.py
+++ mefait #4/main.py
@@ -3,21 +3,33 @@ dos=lambda path: os.mkdir(path)
 file=lambda path : os.system(f'echo pas de solution ici >> "{path}.txt"')
 
 def parcours():
+    " Parcours des data, renvoie la liste "
     qandr=open('qandr.csv')
     ligne=qandr.readline()
     while ligne:
         yield ligne.split(sep=';')
         ligne=qandr.readline()[:-1]
     qandr.close()
+    
 dos("./res")
 path='./res/'
 for brain in parcours():
+    # L'indice de la solution
     good=int(brain[-1])
     brain.pop()
-    if file(path+brain[0]):
-        print(brain)
+
+    # La question en titre de fichier text
+    file(path+brain[0])
     brain.pop(0)
-    for content in brain:
+    
+    for ind,content in enumerate(brain):
         dos(path+content)
+        if ind!=good:
+            os.system('cp hom.lnk '+path+content+"/fin.lnk")
+            # renvoyer vers le début si on se trompe,
+            # la vraie solution finale sera aussi accesible
+            # seulement par un fichier fin.lnk
+    
+    # continuer à la prochaine descendance
     path+=brain[good]+"/"
     
